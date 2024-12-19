33f302178483ba69e728dc673d41d74fd950fb72
Roman Savich <romasavich2002@gmail.com>
2024-12-19 16:22:56 +0300

    refactor get.js

--- server/routes/crud/get/get.js
+++ server/routes/crud/get/get.js
@@ -6,7 +6,10 @@ router.get('/get', async (req, res) => {
 	try {
 		const connection = await getConnection();
 		const vacancyModel = getVacancyModel(connection);
-		const result = await vacancyModel.find();
+		const result = await vacancyModel.find(
+			{},
+			'companyName vacancy note responseStatus salaryRange'
+		);
 		connection.close();
 		return res.status(200).json(result);
 	} catch (error) {
