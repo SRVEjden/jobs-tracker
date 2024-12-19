8b7ff49fff217393fa42a0277895bb576adb3e62
Roman Savich <romasavich2002@gmail.com>
2024-12-19 15:54:57 +0300

    work on delete request

--- server/routes/crud/delete/delete.js
+++ server/routes/crud/delete/delete.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const getConnection = require('../../../db/connection.js');
+const getVacancyModel = require('../../../db/vacancySchema.js');
 const router = express.Router();
 
-router.delete('/delete', (req, res) => {
-	// delete logic here
+router.delete('/delete', async (req, res) => {
+	if (!req?.body) return res.sendStatus(500);
+	try {
+		const { id } = req.body;
+		const connection = await getConnection();
+		const vacancyModel = getVacancyModel(connection);
+		await vacancyModel.findByIdAndDelete(id);
+		connection.close();
+		return res.status(200).send({ id });
+	} catch (error) {
+		console.error(error);
+		return res.sendStatus(500);
+	}
 });
 module.exports = router;
