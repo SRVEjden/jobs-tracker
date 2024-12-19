381388f4b23b182aa7dbc8784cf9ad26bfddac3d
Roman Savich <romasavich2002@gmail.com>
2024-12-19 15:44:29 +0300

    Work on add request

--- server/routes/crud/add/add.js
+++ server/routes/crud/add/add.js
@@ -2,5 +2,26 @@ const express = require('express');
 const router = express.Router();
 const getVacancyModel = require('../../../db/vacancySchema.js');
 const getConnection = require('../../../db/connection.js');
-router.post('/add', async (req, res) => {});
+router.post('/add', async (req, res) => {
+	if (!req.body) {
+		return res.sendStatus(500);
+	}
+	const { companyName, vacancy, note, salaryRange, responseStatus } = req?.body;
+	try {
+		const connection = await getConnection();
+		const vacancyModel = getVacancyModel(connection);
+		const newVacancy = new vacancyModel({
+			companyName,
+			vacancy,
+			note,
+			salaryRange,
+			responseStatus,
+		});
+		await newVacancy.save();
+		connection.close();
+		return res.status(200).json({ id: newVacancy._id });
+	} catch (error) {
+		console.error(error);
+	}
+});
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -4,9 +4,8 @@ const express = require('express');
 const port = process.env.PORT || 5000;
 
 const app = express();
-
-app.use('/', routes);
 app.use(middlewares);
+app.use('/', routes);
 app.listen(port, err => {
 	if (err) {
 		console.error(err);
