f11b1c35f578974b0629b73de402edbcd9b6dfac
Roman Savich <romasavich2002@gmail.com>
2024-12-19 16:15:46 +0300

    work on update request

--- server/routes/crud/update/update.js
+++ server/routes/crud/update/update.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const router = express.Router();
-
-router.post('/update', (req, res) => {
-	//update logic here
+const getConnection = require('../../../db/connection.js');
+const getVacancyModel = require('../../../db/vacancySchema.js');
+router.patch('/update', async (req, res) => {
+	if (!req?.body) {
+		console.log(req.body);
+		return res.sendStatus(500);
+	}
+	try {
+		const { id, companyName, note, responseStatus, vacancy, salaryRange } =
+			req.body;
+		const connection = await getConnection();
+		const vacancyModel = getVacancyModel(connection);
+		await vacancyModel.findByIdAndUpdate(id, {
+			companyName,
+			note,
+			salaryRange,
+			responseStatus,
+			vacancy,
+		});
+		return res.status(200).json({ id });
+	} catch (error) {
+		console.error(error);
+		return res.sendStatus(500);
+	}
 });
 module.exports = router;
