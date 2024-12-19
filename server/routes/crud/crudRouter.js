29b98de4d8ea1b8632d45b90a107d99baae5ff43
Roman Savich <romasavich2002@gmail.com>
2024-12-19 16:04:20 +0300

    work on get request

--- server/routes/crud/crudRouter.js
+++ server/routes/crud/crudRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const addRoute = require('./add/add.js');
 const deleteRoute = require('./delete/delete.js');
 const updateRoute = require('./update/update.js');
-const getRoute = require('./update/update.js');
+const getRoute = require('./get/get.js');
 
 router.use('/', addRoute);
 router.use('/', deleteRoute);
--- server/routes/crud/get/get.js
+++ server/routes/crud/get/get.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
-
-router.get('/get', (req, res) => {
-	//update logic here
+const getConnection = require('../../../db/connection.js');
+const getVacancyModel = require('../../../db/vacancySchema.js');
+router.get('/get', async (req, res) => {
+	try {
+		const connection = await getConnection();
+		const vacancyModel = getVacancyModel(connection);
+		const result = await vacancyModel.find();
+		connection.close();
+		return res.status(200).json(result);
+	} catch (error) {
+		console.error(error);
+		return res.sendStatus(500);
+	}
 });
 module.exports = router;
